92ae916ff930fe4595b3e16e71d3e80af5ef4b4a
Salim Abubakar <samatazu@gmail.com>
2021-04-12 01:54:35 +0100

    Account and settings screen UI implemented

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ import {
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Picker} from '@react-native-picker/picker';
+import Modal from 'react-native-modal';
 
 const Stack = createStackNavigator();
 const windowWidth = Dimensions.get('window').width;
@@ -38,6 +39,41 @@ const Title = () => {
   );
 };
 
+const ProfileEntry = (props) => {
+  return (
+    <>
+      <Text style={{marginBottom: 10}}>{props.first}</Text>
+      <Text style={{color: 'grey', fontSize: 13, marginBottom: 10}}>
+        {props.second}
+      </Text>
+    </>
+  );
+};
+
+const Divider = (props) => {
+  return (
+    <View
+      style={{
+        marginBottom: 10,
+        marginTop: props.marginTop,
+        width: windowWidth,
+        height: 1,
+        backgroundColor: 'lightgrey',
+      }}
+    />
+  );
+};
+
+const SettingsItem = (props) => {
+  return (
+    <View style={{flexDirection: 'row', marginBottom: 10, marginTop: 10}}>
+      <Image style={{marginLeft: 10, marginRight: 25}} source={props.icon} />
+      <Pressable onPress={props.onPress}>
+        <Text style={{marginTop: 0}}>{props.item}</Text>
+      </Pressable>
+    </View>
+  );
+};
 const Input = (props) => {
   return (
     <TextInput
@@ -76,7 +112,6 @@ const ScrollItem = (props) => {
   return (
     <View
       style={{
-        width: 95,
         height: 40,
         borderRadius: 8,
         backgroundColor: '#F5F5F5',
@@ -88,9 +123,11 @@ const ScrollItem = (props) => {
         style={{
           width: 30,
           height: 25,
+          borderRadius: 8,
           backgroundColor: '#f56942',
-          marginTop: 5,
+          marginTop: 7.5,
           marginLeft: 5,
+          marginRight: 5,
         }}
       />
       <Text
@@ -121,7 +158,7 @@ const CategoryView = () => {
 const CardView = () => {
   return (
     <>
-      <View style={{flex: 1, marginBottom: 10, padding: 10}}>
+      <View style={{flex: 1, marginBottom: 10, paddingLeft: 5}}>
         <View
           style={{
             flex: 1,
@@ -179,21 +216,21 @@ const CardView = () => {
             }}>
             <View style={{flexDirection: 'row'}}>
               <Image
-                style={{margin: 10}}
+                style={{margin: 15}}
                 source={require('./src/assets/messag1.png')}
               />
               <Image
-                style={{margin: 10}}
+                style={{margin: 15}}
                 source={require('./src/assets/call.png')}
               />
             </View>
             <View style={{flexDirection: 'row'}}>
               <Image
-                style={{margin: 10}}
+                style={{margin: 15}}
                 source={require('./src/assets/info1.png')}
               />
               <Image
-                style={{margin: 10}}
+                style={{margin: 15}}
                 source={require('./src/assets/save1.png')}
               />
             </View>
@@ -209,7 +246,7 @@ const CardView = () => {
               sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Text>
           </View>
-          <Text style={{alignSelf: 'flex-end', marginRight: 5}}>
+          <Text style={{alignSelf: 'flex-end', marginRight: 5, fontSize: 12}}>
             Jan 9, 2019
           </Text>
         </View>
@@ -261,15 +298,26 @@ const RegisterScreen = (props) => {
               placeholder="Phone Number"
               keyboardType="phone-pad"
             />
+            <Input
+              width={windowWidth - 40}
+              placeholder="School Name"
+              keyboardType="email-address"
+            />
 
-            <Text style={{fontSize: 13, color: 'grey'}}>
+            <Text
+              style={{
+                fontSize: 13,
+                color: 'grey',
+                paddingLeft: 20,
+                paddingTop: 20,
+              }}>
               By creating an account with us, you agree with our terms and
               conditions.
             </Text>
           </View>
           <View>
             <CustomButton
-              onPress={() => props.navigation.navigate('CreateProfile')}
+              onPress={() => props.navigation.navigate('OTPScreen')}
               title="Create Account"
             />
 
@@ -309,11 +357,11 @@ const OTPScreen = (props) => {
             flexDirection: 'row',
             alignSelf: 'center',
           }}>
-          <Input width={66} placeholder="" />
-          <Input width={66} placeholder="" />
-          <Input width={66} placeholder="" />
-          <Input width={66} placeholder="" />
-          <Input width={66} placeholder="" />
+          <Input width={63} placeholder="" />
+          <Input width={63} placeholder="" />
+          <Input width={63} placeholder="" />
+          <Input width={63} placeholder="" />
+          <Input width={63} placeholder="" />
         </View>
         <View style={{alignSelf: 'center', paddingTop: 10}}>
           <CustomButton
@@ -420,37 +468,207 @@ const Home = () => {
   );
 };
 
+const Profile = () => {
+  return (
+    <SafeAreaView style={{flex: 1, padding: 5, backgroundColor: '#ffffff'}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <View style={{flexDirection: 'row'}}>
+          <Image
+            style={{marginLeft: 10, marginRight: 15}}
+            source={require('./src/assets/settings.png')}
+          />
+          <Text>Profile</Text>
+        </View>
+        <Image
+          style={{marginRight: 10}}
+          source={require('./src/assets/settings.png')}
+        />
+      </View>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 50,
+          backgroundColor: 'orangered',
+          alignSelf: 'center',
+        }}
+      />
+      <Text
+        style={{
+          marginTop: 20,
+          alignSelf: 'center',
+          fontSize: 18,
+          fontWeight: 'bold',
+        }}>
+        Fela Kuti
+      </Text>
+      <Divider marginTop={50} />
+      <View style={{padding: 20}}>
+        <Text style={{fontSize: 15, fontWeight: 'bold', marginBottom: 10}}>
+          Personal Information
+        </Text>
+        <ProfileEntry first="First Name" second="Fela" />
+        <ProfileEntry first="Last Name" second="Kuti" />
+        <ProfileEntry first="Email" second="felakuti@gmail.com" />
+        <ProfileEntry first="Mobile" second="+234XXXXXXXXX" />
+        <Divider marginTop={10} />
+        <ProfileEntry first="University" second="University of Lagos, Lagos" />
+        <Divider marginTop={10} />
+        <ProfileEntry first="Bio" second="Blah blah blah blah blah" />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const LogoutModal = (props) => {
+  return (
+    <SafeAreaView>
+      <Modal isVisible={props.isModalVisible}>
+        <View
+          style={{
+            alignSelf: 'center',
+            height: 150,
+            width: windowWidth - 100,
+            borderRadius: 8,
+            backgroundColor: 'white',
+          }}>
+          <Text style={{alignSelf: 'center', padding: 30}}>
+            Dear Fela, are you sure you want to logout
+          </Text>
+          <View style={{marginTop: 0}} />
+          <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+            <Pressable style={{marginTop: 15}}>
+              <Text>Cancel</Text>
+            </Pressable>
+            <Pressable
+              onPress={props.toggleModal}
+              style={{
+                color: 'white',
+                backgroundColor: 'red',
+                width: windowWidth - 300,
+                height: 48,
+                borderRadius: 8,
+              }}>
+              <Text style={{textAlign: 'center', color: 'white', margin: 15}}>
+                Log out
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </SafeAreaView>
+  );
+};
+
+const Settings = () => {
+  const [isModalVisible, setModalVisible] = React.useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+  return (
+    <SafeAreaView style={{flex: 1, padding: 5, backgroundColor: '#ffffff'}}>
+      <View style={{padding: 5}}>
+        <View style={{flexDirection: 'row'}}>
+          <Image
+            style={{marginLeft: 7.5, marginRight: 15}}
+            source={require('./src/assets/settings.png')}
+          />
+          <Text style={{marginTop: 1, fontSize: 16}}>Settings</Text>
+        </View>
+        <View style={{marginBottom: 40}} />
+        <SettingsItem
+          icon={require('./src/assets/user_account.png')}
+          item="Account"
+        />
+        <SettingsItem
+          icon={require('./src/assets/user_message.png')}
+          item="Contact us"
+        />
+
+        <SettingsItem
+          icon={require('./src/assets/user_avatar.png')}
+          item="About us"
+        />
+
+        <SettingsItem
+          icon={require('./src/assets/user_info.png')}
+          item="App info"
+        />
+
+        <SettingsItem
+          style={{color: 'red'}}
+          icon={require('./src/assets/user_logout.png')}
+          item="Log out"
+          onPress={() => toggleModal()}
+        />
+
+        <LogoutModal
+          isModalVisible={isModalVisible}
+          toggleModal={toggleModal}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const Account = () => {
+  return (
+    <SafeAreaView style={{flex: 1, padding: 5, backgroundColor: '#ffffff'}}>
+      <View style={{padding: 5}}>
+        <View style={{flexDirection: 'row'}}>
+          <Image
+            style={{marginLeft: 7.5, marginRight: 15}}
+            source={require('./src/assets/settings.png')}
+          />
+          <Text style={{marginTop: 1, fontSize: 16}}>Account</Text>
+        </View>
+        <View style={{marginBottom: 40}} />
+        <View style={{marginLeft: 10}}>
+          <Text>Terms & privacy policy</Text>
+          <View style={{marginBottom: 20}} />
+          <Text style={{color: 'red'}}>Delete account</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="SplashScreen"
-          component={SplashScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="RegisterScreen"
-          component={RegisterScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="OTPScreen"
-          component={OTPScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="CreateProfile"
-          component={CreateProfile}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Settings />
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen
+    //       name="SplashScreen"
+    //       component={SplashScreen}
+    //       options={{headerShown: false}}
+    //     />
+    //     <Stack.Screen
+    //       name="RegisterScreen"
+    //       component={RegisterScreen}
+    //       options={{headerShown: false}}
+    //     />
+    //     <Stack.Screen
+    //       name="OTPScreen"
+    //       component={OTPScreen}
+    //       options={{headerShown: false}}
+    //     />
+    //     <Stack.Screen
+    //       name="CreateProfile"
+    //       component={CreateProfile}
+    //       options={{headerShown: false}}
+    //     />
+    //     <Stack.Screen
+    //       name="Home"
+    //       component={Home}
+    //       options={{headerShown: false}}
+    //     />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 };
 
